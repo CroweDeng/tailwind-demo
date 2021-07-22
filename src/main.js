be15736e8287e07bd90d230c583cc8a53eb066e5
Crowe Deng <crowe.deng@neutec.comtw>
2021-07-22 12:03:11 +0800

    Revise: 修正tailwindcss 用sass loader compile

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import './tailwindcss/index.css';
+import './tailwindcss/index.scss';
 
 createApp(App)
   .use(router)
